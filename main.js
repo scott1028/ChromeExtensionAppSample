11a8cfc90d493b56efee1e92f80b509cbc9981e4
scott1028 <mic1028002@gmail.com>
2013-12-03 10:12:28 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+/*
+	Content Script 在一個很特殊的環境下執行，他可以存取當前頁面的 Dom 物件但是又不能存取當前頁面額外的 JavaScript 變數、函數
+*/
+
+// 如果你想寫翻譯外掛，相信這個會對你有用的！
+document.addEventListener('mouseup',function(e){
+	console.log( window.getSelection().toString() );
+});
