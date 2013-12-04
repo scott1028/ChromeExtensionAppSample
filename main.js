abd2233803c90ff7e73a7bdaac1c426f91d2d132
scott1028 <mic1028002@gmail.com>
2013-12-04 11:07:12 +0800

    modified:   main.js 	modified:   manifest.json

--- main.js
+++ main.js
@@ -2,11 +2,16 @@
 	Content Script 在一個很特殊的環境下執行，他可以存取當前頁面的 Dom 物件但是又不能存取當前頁面額外的 JavaScript 變數、函數
 */
 
-// 如果你想寫翻譯外掛，相信這個會對你有用的！
-document.addEventListener('mouseup',function(e){
-	console.log( window.getSelection().toString() );
+$(document).ready(function(e){
 
-	// 似乎不用偵測 JQuery 是否準備好
-	var thisPageAsText=$(document).find('body').text();
-	console.log(thisPageAsText);
+	console.log('JQuery is Finished!',$);
+
+	// 如果你想寫翻譯外掛，相信這個會對你有用的！
+	document.addEventListener('mouseup',function(e){
+		console.log( window.getSelection().toString() );
+
+		// 似乎不用偵測 JQuery 是否準備好
+		var thisPageAsText=$(document).find('body').text();
+		console.log(thisPageAsText);
+	});
 });
