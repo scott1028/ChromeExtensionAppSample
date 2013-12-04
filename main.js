e2aa3f78f68cb48b3b7772f9707e87f833689f97
scott1028 <mic1028002@gmail.com>
2013-12-04 11:09:49 +0800

    modified:   main.js

--- main.js
+++ main.js
@@ -3,15 +3,19 @@
 */
 
 $(document).ready(function(e){
-
+	// 驗證 JQuery
 	console.log('JQuery is Finished!',$);
 
 	// 如果你想寫翻譯外掛，相信這個會對你有用的！
 	document.addEventListener('mouseup',function(e){
+		// 得到目前框選文字的 Handle
 		console.log( window.getSelection().toString() );
 
 		// 似乎不用偵測 JQuery 是否準備好
 		var thisPageAsText=$(document).find('body').text();
 		console.log(thisPageAsText);
+
+		// 可以用此方法做出網路辭典
+		$('body').append($('<p>'+window.getSelection().toString()+'</p>'));
 	});
 });
