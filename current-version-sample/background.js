c59d6708be43ea9e91f98aa001ef0a593682913f
scott.lan <scott.lan@taisys.com>
2016-09-02 17:04:58 +0800

    update code

---
+++ current-version-sample/background.js
@@ -0,0 +1,48 @@
+chrome.app.runtime.onLaunched.addListener(function() {
+  chrome.app.window.create('window.html', {
+    'outerBounds': {
+      'width': 400,
+      'height': 500
+    }
+  });
+
+  function registerCallback(registrationId) {
+    console.log(registrationId);
+    if (chrome.runtime.lastError) {
+      // When the registration fails, handle the error and retry the
+      // registration later.
+      return;
+    }
+
+    // Send the registration token to your application server.
+    sendRegistrationId(function(succeed) {
+      // Once the registration token is received by your server,
+      // set the flag such that register will not be invoked
+      // next time when the app starts up.
+      if (succeed)
+        chrome.storage.local.set({registered: true});
+    });
+  }
+
+  function sendRegistrationId(callback) {
+    // Send the registration token to your application server
+    // in a secure way.
+  }
+
+  chrome.storage.local.get("registered", function(result) {
+    // If already registered, bail out.
+    if (result["registered"])
+      return;
+
+    // Up to 100 senders are allowed.
+    var senderIds = ["979397196554"];
+    chrome.gcm.register(senderIds, registerCallback);
+  });
+
+  chrome.gcm.onMessage.addListener(function(message) {
+    // A message is an object with a data property that
+    // consists of key-value pairs.
+    console.log(message);
+  });
+
+});
